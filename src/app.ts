7fce2265db8be19da97c606a484d6e9b3774f81e
PhePheMath <phelyppemath@outlook.com>
2021-10-21 13:42:42 -0300

    Getting rid of background login

--- src/app.ts
+++ src/app.ts
@@ -21,14 +21,4 @@ io.on("connection", (socket) => console.log(`Usuário connectado no socket ${soc
 app.use(express.json())
 app.use(router)
 
-app.get("/github", (req, res) => {
-    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
-
-app.get("/signin/callback", (req, res) => {
-    const { code } = req.query;
-
-    return res.json(code)
-})
-
 export { serverHttp, io };
